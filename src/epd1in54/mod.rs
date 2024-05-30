caemor/epd-waveshare

do not use polling to wait for idle

src/epd1in54/mod.rs
@@ -55,7 +55,7 @@ const IS_BUSY_LOW: bool = false;
 const SINGLE_BYTE_WRITE: bool = true;
 
 use embedded_hal::digital::{InputPin, OutputPin};
-use embedded_hal_async::{delay::DelayUs, spi::SpiDevice};
+use embedded_hal_async::{delay::DelayUs, digital::Wait, spi::SpiDevice};
 
 use crate::type_a::{
     command::Command,
@@ -92,7 +92,7 @@ pub struct Epd1in54<SPI, BUSY, DC, RST, DELAY> {
 impl<SPI, BUSY, DC, RST, DELAY> Epd1in54<SPI, BUSY, DC, RST, DELAY>
 where
     SPI: SpiDevice,
-    BUSY: InputPin,
+    BUSY: InputPin + Wait,
     DC: OutputPin,
     RST: OutputPin,
     DELAY: DelayUs,
@@ -154,7 +154,7 @@ impl<SPI, BUSY, DC, RST, DELAY> WaveshareDisplay<SPI, BUSY, DC, RST, DELAY>
     for Epd1in54<SPI, BUSY, DC, RST, DELAY>
 where
     SPI: SpiDevice,
-    BUSY: InputPin,
+    BUSY: InputPin + Wait,
     DC: OutputPin,
     RST: OutputPin,
     DELAY: DelayUs,
@@ -315,7 +315,7 @@ where
 impl<SPI, BUSY, DC, RST, DELAY> Epd1in54<SPI, BUSY, DC, RST, DELAY>
 where
     SPI: SpiDevice,
-    BUSY: InputPin,
+    BUSY: InputPin + Wait,
     DC: OutputPin,
     RST: OutputPin,
     DELAY: DelayUs,

src/interface.rs
@@ -1,8 +1,7 @@
 use crate::traits::Command;
 use core::marker::PhantomData;
-use embedded_hal::digital::{InputPin, OutputPin};
-use embedded_hal_async::{delay::DelayUs, spi::SpiDevice};
-
+use embedded_hal::digital::{InputPin, OutputPin, ErrorType};
+use embedded_hal_async::{delay::DelayUs, spi::SpiDevice, digital::Wait};
 /// The Connection Interface of all (?) Waveshare EPD-Devices
 ///
 /// SINGLE_BYTE_WRITE defines if a data block is written bytewise
@@ -26,7 +25,7 @@ impl<SPI, BUSY, DC, RST, DELAY, const SINGLE_BYTE_WRITE: bool>
     DisplayInterface<SPI, BUSY, DC, RST, DELAY, SINGLE_BYTE_WRITE>
 where
     SPI: SpiDevice,
-    BUSY: InputPin,
+    BUSY: InputPin + Wait,
     DC: OutputPin,
     RST: OutputPin,
     DELAY: DelayUs,
@@ -139,18 +138,20 @@ where
     ///  - FALSE for epd2in9, epd1in54 (for all Display Type A ones?)
     ///
     /// Most likely there was a mistake with the 2in9 busy connection
-    pub(crate) async fn wait_until_idle(&mut self, delay: &mut DELAY, is_busy_low: bool) {
-        while self.is_busy(is_busy_low) {
-            // This has been removed and added many time :
-            // - it is faster to not have it
-            // - it is complicated to pass the delay everywhere all the time
-            // - busy waiting can consume more power that delaying
-            // - delay waiting enables task switching on realtime OS
-            // -> keep it and leave the decision to the user
-            if self.delay_us > 0 {
-                delay.delay_us(self.delay_us).await;
-            }
+    pub(crate) async fn wait_until_idle(&mut self, delay: &mut DELAY, is_busy_low: bool) -> Result<(), BUSY::Error> {
+
+        if is_busy_low {
+            self.busy.wait_for_high().await?;
         }
+        else {
+            self.busy.wait_for_low().await?;
+        }
+
+        // TODO: remove
+        if self.delay_us > 0 {
+            delay.delay_us(self.delay_us).await;
+        }
+        Ok(())
     }
 
     /// Same as `wait_until_idle` for device needing a command to probe Busy pin
@@ -165,6 +166,7 @@ where
         if self.delay_us > 0 {
             delay.delay_us(self.delay_us).await;
         }
+
         while self.is_busy(is_busy_low) {
             self.cmd(spi, status_command).await?;
             if self.delay_us > 0 {

vhdirk/epd-waveshare-async

src/epd1in54/mod.rs
@@ -55,7 +55,7 @@ const IS_BUSY_LOW: bool = false;
 const SINGLE_BYTE_WRITE: bool = true;
 
 use embedded_hal::digital::{InputPin, OutputPin};
-use embedded_hal_async::{delay::DelayNs, spi::SpiDevice};
+use embedded_hal_async::{delay::DelayNs, digital::Wait, spi::SpiDevice};
 
 use crate::type_a::{
     command::Command,
@@ -92,7 +92,7 @@ pub struct Epd1in54<SPI, BUSY, DC, RST, DELAY> {
 impl<SPI, BUSY, DC, RST, DELAY> Epd1in54<SPI, BUSY, DC, RST, DELAY>
 where
     SPI: SpiDevice,
-    BUSY: InputPin,
+    BUSY: InputPin + Wait,
     DC: OutputPin,
     RST: OutputPin,
     DELAY: DelayNs,
@@ -154,7 +154,7 @@ impl<SPI, BUSY, DC, RST, DELAY> WaveshareDisplay<SPI, BUSY, DC, RST, DELAY>
     for Epd1in54<SPI, BUSY, DC, RST, DELAY>
 where
     SPI: SpiDevice,
-    BUSY: InputPin,
+    BUSY: InputPin + Wait,
     DC: OutputPin,
     RST: OutputPin,
     DELAY: DelayNs,
@@ -315,7 +315,7 @@ where
 impl<SPI, BUSY, DC, RST, DELAY> Epd1in54<SPI, BUSY, DC, RST, DELAY>
 where
     SPI: SpiDevice,
-    BUSY: InputPin,
+    BUSY: InputPin + Wait,
     DC: OutputPin,
     RST: OutputPin,
     DELAY: DelayNs,

src/interface.rs
@@ -1,7 +1,7 @@
 use crate::traits::Command;
 use core::marker::PhantomData;
-use embedded_hal::digital::{InputPin, OutputPin};
-use embedded_hal_async::{delay::DelayNs, spi::SpiDevice};
+use embedded_hal::digital::{InputPin, OutputPin, ErrorType};
+use embedded_hal_async::{delay::DelayNs, spi::SpiDevice, digital::Wait};
 
 /// The Connection Interface of all (?) Waveshare EPD-Devices
 ///
@@ -26,7 +26,7 @@ impl<SPI, BUSY, DC, RST, DELAY, const SINGLE_BYTE_WRITE: bool>
     DisplayInterface<SPI, BUSY, DC, RST, DELAY, SINGLE_BYTE_WRITE>
 where
     SPI: SpiDevice,
-    BUSY: InputPin,
+    BUSY: InputPin + Wait,
     DC: OutputPin,
     RST: OutputPin,
     DELAY: DelayNs,
@@ -139,18 +139,20 @@ where
     ///  - FALSE for epd2in9, epd1in54 (for all Display Type A ones?)
     ///
     /// Most likely there was a mistake with the 2in9 busy connection
-    pub(crate) async fn wait_until_idle(&mut self, delay: &mut DELAY, is_busy_low: bool) {
-        while self.is_busy(is_busy_low) {
-            // This has been removed and added many time :
-            // - it is faster to not have it
-            // - it is complicated to pass the delay everywhere all the time
-            // - busy waiting can consume more power that delaying
-            // - delay waiting enables task switching on realtime OS
-            // -> keep it and leave the decision to the user
-            if self.delay_us > 0 {
-                delay.delay_us(self.delay_us).await;
-            }
+    pub(crate) async fn wait_until_idle(&mut self, delay: &mut DELAY, is_busy_low: bool) -> Result<(), BUSY::Error> {
+
+        if is_busy_low {
+            self.busy.wait_for_high().await?;
         }
+        else {
+            self.busy.wait_for_low().await?;
+        }
+
+        // TODO: remove
+        if self.delay_us > 0 {
+            delay.delay_us(self.delay_us).await;
+        }
+        Ok(())
     }
 
     /// Same as `wait_until_idle` for device needing a command to probe Busy pin
@@ -165,6 +167,7 @@ where
         if self.delay_us > 0 {
             delay.delay_us(self.delay_us).await;
         }
+
         while self.is_busy(is_busy_low) {
             self.cmd(spi, status_command).await?;
             if self.delay_us > 0 {